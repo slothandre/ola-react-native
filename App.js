94b8ceecdab83494d13f5abff8e9c8af18183ed8
André Marques <andre.m.d.abreu@gmail.com>
2024-02-07 10:04:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const vai = () => {
+    Alert.alert("Titulo da janela", "Bom dia familia!");
+  };
+
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <View>
-        <Text>Olá React Native!</Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text>Topo/Cabeçalho</Text>
+        </View>
+        <View>
+          <Text>Conteúdo...</Text>
+        </View>
+        <View>
+          <Text>Rodapé...</Text>
+          <Button title="Toca aqui!!!" onPress={vai} />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
